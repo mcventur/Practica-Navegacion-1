ce6902b5e202cb8ec329d785ce1ed60d9f9b3982
mcventur <marcelinopenide@gmail.com>
2023-11-08 10:49:49 +0100

    Layout Bombilla con fragments se lanza

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mpd.pmdm.practicanavegacion1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mpd.pmdm.practicanavegacion1"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/mpd/pmdm/practicanavegacion1/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/practicanavegacion1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.mpd.pmdm.practicanavegacion1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mpd.pmdm.practicanavegacion1.bombilla.BombillaActivity
+import com.mpd.pmdm.practicanavegacion1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAppBombilla.setOnClickListener {
+            val appBombillaIntent = Intent(this, BombillaActivity::class.java)
+            startActivity(appBombillaIntent)
+        }
     }
 }
