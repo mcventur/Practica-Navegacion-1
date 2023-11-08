5f1a81505e7eb58d2b63b1acdc90cb5c914990fd
mcventur <marcelinopenide@gmail.com>
2023-11-08 10:14:37 +0100

    Actividades Bombilla y Semaforo vacias

---
+++ app/src/main/java/com/mpd/pmdm/practicanavegacion1/bombilla/BombillaActivity.kt
@@ -0,0 +1,12 @@
+package com.mpd.pmdm.practicanavegacion1.bombilla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mpd.pmdm.practicanavegacion1.R
+
+class BombillaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bombilla)
+    }
+}
---
+++ app/src/main/java/com/mpd/pmdm/practicanavegacion1/semaforo/SemaforoActivity.kt
@@ -0,0 +1,12 @@
+package com.mpd.pmdm.practicanavegacion1.semaforo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mpd.pmdm.practicanavegacion1.R
+
+class SemaforoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_semaforo)
+    }
+}
